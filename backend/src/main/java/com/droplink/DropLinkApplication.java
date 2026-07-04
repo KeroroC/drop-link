e4af25d30083eba2a9e01572f72ccdb8598e852d
keroro <keroro714@qq.com>
2026-07-04 15:59:27 +0800

    feat: 初始化 Spring Boot 后端项目

---
+++ backend/src/main/java/com/droplink/DropLinkApplication.java
@@ -0,0 +1,11 @@
+package com.droplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DropLinkApplication.class, args);
+    }
+}
